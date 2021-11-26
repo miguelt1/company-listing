a8dac6bbdbb6f05ffb5f5c329fa9973f72dc8e7e
miguel.tavares <miguel.tavares@pipecodes.com>
2021-11-26 12:42:00 +0000

    last adjustments

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 
-import App from './App';
+import App from './App'
 
 const element = (
   <BrowserRouter>
     <App />
   </BrowserRouter>
-);
+)
 
-const container = document.getElementById('root');
+const container = document.getElementById('root')
 
-ReactDOM.render(element, container);
+ReactDOM.render(element, container)
